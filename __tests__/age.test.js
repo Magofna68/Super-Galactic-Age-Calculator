23c46d237464ec48c34e70523208b2205ab1ea2b
Brandon-Magofna <magofna68@gmail.com>
2021-04-16 09:03:39 -0700

    successfully pass first test for contructor function

--- __tests__/age.test.js
+++ __tests__/age.test.js
@@ -1,14 +1,15 @@
-import { TestScheduler } from '@jest/core';
-import { describe } from 'yargs'
 import { Age } from '../src/age.js'
 
 describe('Age', () => {
   let myAge;
-  beforeEach() => {
+  beforeEach(() => {
     myAge = new Age()
   })
 
   test('Should properly create an object with Age constructor', () => {
+    // const myAge = new Age()
     expect(myAge).toBeDefined();
-  }
+  })
+
+
 })
